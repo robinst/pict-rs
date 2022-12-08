02526ffce8c01f52ee08654ed4f0534e206afb10
asonix <asonix@asonix.dog>
2022-12-07 18:32:02 -0600

    Remove unused docker-related files

--- docker/dev/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ARCH=${1:-amd64}
-
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-
-sudo docker-compose build --pull
-sudo docker-compose run --service-ports pictrs-$ARCH
