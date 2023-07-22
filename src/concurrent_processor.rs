8d35c2449d829fbf2da2b9f54675b901eb7e3345
asonix <asonix@asonix.dog>
2023-07-22 10:44:43 -0500

    Simplify the cancel-safe processor

--- src/concurrent_processor.rs
+++ src/concurrent_processor.rs
@@ -4,6 +4,7 @@ use crate::{
 };
 use actix_web::web;
 use dashmap::{mapref::entry::Entry, DashMap};
+use flume::{r#async::RecvFut, Receiver, Sender};
 use once_cell::sync::Lazy;
 use std::{
     future::Future,
@@ -11,21 +12,35 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
 };
-use tokio::sync::oneshot::{Receiver, Sender};
 use tracing::Span;
 
-type OutcomeSender = Sender<(Details, web::Bytes)>;
+type OutcomeReceiver = Receiver<(Details, web::Bytes)>;
 
 type ProcessMapKey = (Vec<u8>, PathBuf);
 
-type ProcessMap = DashMap<ProcessMapKey, Vec<OutcomeSender>>;
+type ProcessMap = DashMap<ProcessMapKey, OutcomeReceiver>;
 
 static PROCESS_MAP: Lazy<ProcessMap> = Lazy::new(DashMap::new);
 
 struct CancelToken {
     span: Span,
     key: ProcessMapKey,
-    receiver: Option<Receiver<(Details, web::Bytes)>>,
+    state: CancelState,
+}
+
+enum CancelState {
+    Sender {
+        sender: Sender<(Details, web::Bytes)>,
+    },
+    Receiver {
+        receiver: RecvFut<'static, (Details, web::Bytes)>,
+    },
+}
+
+impl CancelState {
+    const fn is_sender(&self) -> bool {
+        matches!(self, Self::Sender { .. })
+    }
 }
 
 pin_project_lite::pin_project! {
@@ -44,39 +59,38 @@ where
     pub(super) fn new(hash: &[u8], path: PathBuf, fut: F) -> Self {
         let key = (hash.to_vec(), path.clone());
 
+        let (sender, receiver) = flume::bounded(1);
+
         let entry = PROCESS_MAP.entry(key.clone());
 
-        let (receiver, span) = match entry {
+        let (state, span) = match entry {
             Entry::Vacant(vacant) => {
-                vacant.insert(Vec::new());
+                vacant.insert(receiver);
                 let span = tracing::info_span!(
                     "Processing image",
                     hash = &tracing::field::debug(&hex::encode(hash)),
                     path = &tracing::field::debug(&path),
                     completed = &tracing::field::Empty,
                 );
-                (None, span)
+                (CancelState::Sender { sender }, span)
             }
-            Entry::Occupied(mut occupied) => {
-                let (tx, rx) = tracing::trace_span!(parent: None, "Create channel")
-                    .in_scope(tokio::sync::oneshot::channel);
-
-                occupied.get_mut().push(tx);
+            Entry::Occupied(receiver) => {
                 let span = tracing::info_span!(
                     "Waiting for processed image",
                     hash = &tracing::field::debug(&hex::encode(hash)),
                     path = &tracing::field::debug(&path),
                 );
-                (Some(rx), span)
+                (
+                    CancelState::Receiver {
+                        receiver: receiver.get().clone().into_recv_async(),
+                    },
+                    span,
+                )
             }
         };
 
         CancelSafeProcessor {
-            cancel_token: CancelToken {
-                span,
-                key,
-                receiver,
-            },
+            cancel_token: CancelToken { span, key, state },
             fut,
         }
     }
@@ -92,35 +106,30 @@ where
         let this = self.as_mut().project();
 
         let span = &this.cancel_token.span;
-        let receiver = &mut this.cancel_token.receiver;
+        let state = &mut this.cancel_token.state;
         let key = &this.cancel_token.key;
         let fut = this.fut;
 
-        span.in_scope(|| {
-            if let Some(ref mut rx) = receiver {
-                Pin::new(rx)
-                    .poll(cx)
-                    .map(|res| res.map_err(|_| UploadError::Canceled.into()))
-            } else {
-                fut.poll(cx).map(|res| {
-                    let opt = PROCESS_MAP.remove(key);
-                    res.map(|tup| {
-                        if let Some((_, vec)) = opt {
-                            for sender in vec {
-                                let _ = sender.send(tup.clone());
-                            }
-                        }
-                        tup
-                    })
-                })
-            }
+        span.in_scope(|| match state {
+            CancelState::Sender { sender } => fut.poll(cx).map(|res| {
+                PROCESS_MAP.remove(key);
+
+                if let Ok(tup) = &res {
+                    let _ = sender.try_send(tup.clone());
+                }
+
+                res
+            }),
+            CancelState::Receiver { ref mut receiver } => Pin::new(receiver)
+                .poll(cx)
+                .map(|res| res.map_err(|_| UploadError::Canceled.into())),
         })
     }
 }
 
 impl Drop for CancelToken {
     fn drop(&mut self) {
-        if self.receiver.is_none() {
+        if self.state.is_sender() {
             let completed = PROCESS_MAP.remove(&self.key).is_none();
             self.span.record("completed", completed);
         }
