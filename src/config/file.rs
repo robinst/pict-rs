13387dec4349273914e8a3d5d8de92921ca6864b
asonix <asonix@asonix.dog>
2023-07-22 17:58:03 -0500

    Fix specs

--- src/config/file.rs
+++ src/config/file.rs
@@ -15,6 +15,7 @@ pub(crate) struct ConfigFile {
 
     pub(crate) tracing: Tracing,
 
+    #[serde(default)]
     pub(crate) metrics: Metrics,
 
     pub(crate) old_db: OldDb,
@@ -121,7 +122,7 @@ pub(crate) struct Tracing {
     pub(crate) opentelemetry: OpenTelemetry,
 }
 
-#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+#[derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize)]
 #[serde(rename_all = "snake_case")]
 pub(crate) struct Metrics {
     #[serde(skip_serializing_if = "Option::is_none")]
