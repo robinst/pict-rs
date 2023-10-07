71fbc4c55b1c22f174ed2f13d1a5f83f0b22ce8f
asonix <asonix@asonix.dog>
2023-10-07 12:31:05 -0500

    Just have different mains

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-fn main() -> color_eyre::Result<()> {
-    run()
-}
-
 #[cfg(feature = "io-uring")]
-fn run() -> color_eyre::Result<()> {
+fn main() -> color_eyre::Result<()> {
     tokio_uring::start(async move {
         pict_rs::PictRsConfiguration::build_default()?
             .install_tracing()?
@@ -14,7 +10,7 @@ fn run() -> color_eyre::Result<()> {
 }
 
 #[cfg(not(feature = "io-uring"))]
-fn run() -> color_eyre::Result<()> {
+fn main() -> color_eyre::Result<()> {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()?
